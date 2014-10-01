5e36b430ba51d63730ccab9bad3a0fa124d87ef5
Bruno Galvao <brunopgalvao@gmail.com>
2014-10-01 14:22:14 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   
+  resources :users
+  
   root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
