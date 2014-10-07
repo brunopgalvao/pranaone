371029ea799ee781864a894780fb663ba5932f60
Bruno Galvao <brunopgalvao@gmail.com>
2014-10-07 17:19:29 -0400

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'signup'  => 'users#new'
+  get    'sessions/new'
 
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
   
   resources :users
   
