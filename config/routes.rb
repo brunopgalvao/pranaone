cabf03b7b13b18e4adc614cbac4023e7201b6358
Bruno Galvao <brunopgalvao@gmail.com>
2014-10-15 20:15:47 -0400

    Add account activation, password resets & email configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'account_activations/edit'
+
   get    'sessions/new'
 
   get    'signup'  => 'users#new'
@@ -11,6 +17,8 @@ Rails.application.routes.draw do
   get    'about'   => 'static_pages#about'
   
   resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   
   root 'static_pages#home'
 
